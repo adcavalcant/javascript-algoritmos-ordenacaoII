482485525c57f76821445c37632ae00862a665fb
adcavalcant <aderc19@gmail.com>
2023-07-27 13:46:32 -0300

    Entendendo a recursividade

--- aula-2/index.js
+++ aula-2/index.js
@@ -1,10 +1,13 @@
 const listaLivros = require("./array");
 
-function mergeSort(arr) {
+function mergeSort(arr, nivelAninhamento = 0) {
+  console.log(`Nível de Aninhamento: ${nivelAninhamento}`);
+  console.log(arr);
+
   if (arr.length > 1) {
     const meio = Math.floor(arr.length / 2);
-    const parte1 = mergeSort(arr.slice(0, meio));
-    const parte2 = mergeSort(arr.slice(meio, arr.length));
+    const parte1 = mergeSort(arr.slice(0, meio), nivelAninhamento++); //recursividade
+    const parte2 = mergeSort(arr.slice(meio, arr.length), nivelAninhamento++); //recursividade
     arr = ordena(parte1, parte2);
   }
   return arr;
@@ -15,7 +18,10 @@ function ordena(parte1, parte2) {
   let posicaoAtualParte2 = 0;
   const resultado = [];
 
-  while (posicaoAtualParte1 < parte1.length && posicaoAtualParte2 < parte2.length) {
+  while (
+    posicaoAtualParte1 < parte1.length &&
+    posicaoAtualParte2 < parte2.length
+  ) {
     let produtoAtualParte1 = parte1[posicaoAtualParte1];
     let produtoAtualParte2 = parte2[posicaoAtualParte2];
 
@@ -28,7 +34,11 @@ function ordena(parte1, parte2) {
     }
   }
 
-  return resultado.concat(posicaoAtualParte1 < parte1.length ? parte1.slice(posicaoAtualParte1): parte2.slice(posicaoAtualParte2));
+  return resultado.concat(
+    posicaoAtualParte1 < parte1.length
+      ? parte1.slice(posicaoAtualParte1)
+      : parte2.slice(posicaoAtualParte2)
+  );
 }
 
 console.log(mergeSort(listaLivros));
