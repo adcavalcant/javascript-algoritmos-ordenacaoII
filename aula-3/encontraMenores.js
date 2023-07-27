18caecaba5844c000a5c4a2359ad2bf66a341d8d
adcavalcant <aderc19@gmail.com>
2023-07-27 14:59:08 -0300

    Posicionando um pivô

---
+++ aula-3/encontraMenores.js
@@ -0,0 +1,23 @@
+const listaLivros = require("./array");
+
+function encontraMenores(pivo, array) {
+  let menores = 0;
+  for (let atual = 0; atual < array.length; atual++) {
+    let produtoAtual = array[atual];
+    if (produtoAtual.preco < pivo.preco) {
+      menores++;
+    }
+  }
+  //return menores; //encontra n elementos que são menores que o pivô escolhido.
+  trocaLugar(array, array.indexOf(pivo), menores);
+  return array;
+}
+
+function trocaLugar(array, de, para) {
+  const elem1 = array[de];
+  const elem2 = array[para];
+  array[para] = elem1;
+  array[de] = elem2;
+}
+
+console.log(encontraMenores(listaLivros[2], listaLivros));
